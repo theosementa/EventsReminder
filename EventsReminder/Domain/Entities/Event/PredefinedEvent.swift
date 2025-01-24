594a8e72975321dcd69888db8380c240523d122d
Theo Sementa <theosementa@gmail.com>
2025-01-24 16:42:20 +0100

    feat: add predefined entity

---
+++ EventsReminder/Domain/Entities/Event/PredefinedEvent.swift
@@ -0,0 +1,15 @@
+//
+//  Event+Predefined.swift
+//  EventsReminder
+//
+//  Created by Theo Sementa on 24/01/2025.
+//
+
+import Foundation
+
+struct PrefefinedEvent {
+    var name: String
+    var date: Date
+    var repeatType: Repeat
+    var tag: PredefinedTag
+}
---
+++ EventsReminder/Domain/Entities/Tag/PredefinedTag.swift
@@ -0,0 +1,15 @@
+//
+//  PredefinedTag.swift
+//  EventsReminder
+//
+//  Created by Theo Sementa on 24/01/2025.
+//
+
+import Foundation
+import SwiftUICore
+
+struct PredefinedTag {
+    var name: String
+    var emoji: String
+    var color: Color
+}
