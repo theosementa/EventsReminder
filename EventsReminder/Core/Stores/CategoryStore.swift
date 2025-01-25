100974f6ec22d7579004aba3f564ba9faeef3e18
Theo Sementa <theosementa@gmail.com>
2025-01-25 20:37:36 +0100

    feat: start add CategoryStore

---
+++ EventsReminder/Core/Stores/CategoryStore.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryStore.swift
+//  EventsReminder
+//
+//  Created by Theo Sementa on 25/01/2025.
+//
+
+import Foundation
+
+final class CategoryStore: ObservableObject {
+    static let shared = CategoryStore()
+}
